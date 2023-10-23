db8d372e74bd943aee82f52cacda3444449f82c8
TOAN TRAN <chitoantran@outlook.com>
2023-10-23 13:49:45 +0700

    aider: Create a server.js file that uses the templates/index.html as the main page using the Node.js library.
    # Aider chat conversation:
    
    USER: create a server.js file that use the tempaltes/index.htmnl as main page using nodejs library
    
    ASSISTANT: No changes are needed.

---
+++ server.js
@@ -0,0 +1,26 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const server = http.createServer((req, res) => {
+  // Set the content type to HTML
+  res.setHeader('Content-Type', 'text/html');
+
+  // Read the index.html file
+  fs.readFile(path.join(__dirname, 'templates', 'index.html'), 'utf8', (err, data) => {
+    if (err) {
+      // If there was an error reading the file, send a 500 status code
+      res.statusCode = 500;
+      res.end('Internal Server Error');
+    } else {
+      // Send the contents of the index.html file as the response
+      res.statusCode = 200;
+      res.end(data);
+    }
+  });
+});
+
+const port = 3000;
+server.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
