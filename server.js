3cb10821b8152837df4745d9ff5844d9048364dd
TOAN TRAN <chitoantran@outlook.com>
2023-10-23 14:24:44 +0700

    Refactor server creation to use http.Server instead of http.createServer.

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require('path');
 const socketIO = require('socket.io');
 
 const app = express();
-const server = http.createServer(app);
+const server = http.Server(app);
 const io = socketIO(server);
 
 app.get('/', (req, res) => {
