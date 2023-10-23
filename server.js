0ede044c8ee42b94118a11d4794b9bbf54622e90
TOAN TRAN <chitoantran@outlook.com>
2023-10-23 14:41:28 +0700

    aider: Changed console log message to include the URL of the server.
    # Aider chat conversation:
    
    ASSISTANT: No changes are needed.

--- server.js
+++ server.js
@@ -34,5 +34,5 @@ io.on('connection', (socket) => {
 
 const port = 3000;
 server.listen(port, () => {
-  console.log(`Server is running at on port ${port}`);
+  console.log(`Server is running at http://localhost:${port}`);
 });
